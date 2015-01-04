89b0357bc89f11452bd484910e41d7491ddbcc19
oleegholm <oleegholm@gmail.com>
2015-01-04 20:31:29 +0100

    added txt

--- src/main/scala/com/siigna/web/CanvasView.scala
+++ src/main/scala/com/siigna/web/CanvasView.scala
@@ -84,7 +84,7 @@ class CanvasView(canvas : HTMLCanvasElement) extends Printer {
     context.closePath()
   }
 
-  override def text(x: Double, y: Double, h: Double, t: String): Unit = {
+  override def text(x: Double, y: Double, h: Double, t: Any): Unit = {
     val correctedH = h / 1.5
     val myFont = correctedH.toString() + "px Arial"
     context.font = myFont
