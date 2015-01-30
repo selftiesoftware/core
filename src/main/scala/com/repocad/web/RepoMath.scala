3b24d7cbeb8302051adb03a8a16199912b1423aa
Ole Egholm <oleegholm@gmail.com>
2015-01-30 10:08:39 +0100

    added tan

--- src/main/scala/com/repocad/web/RepoMath.scala
+++ src/main/scala/com/repocad/web/RepoMath.scala
@@ -9,7 +9,8 @@ object RepoMath {
 
   val toEnv : Evaluator.Env = Map(
     "cos" -> ((p : Printer, degrees : Double) => math.cos(degrees)),
-    "sin" -> ((p : Printer, degrees : Double) => math.sin(degrees))
+    "sin" -> ((p : Printer, degrees : Double) => math.sin(degrees)),
+    "tan" -> ((p : Printer, degrees : Double) => math.tan(degrees))
   )
 
 }
