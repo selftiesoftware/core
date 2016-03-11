6158f2f1d73eb6aa01da2af20f8bbf2eeaa70fd0
Jegp <jensep@gmail.com>
2016-03-11 08:29:48 +0100

    Added a method to create a modifier keys from a modifier key event

--- src/main/scala/com/repocad/util/event/ModifierKeys.scala
+++ src/main/scala/com/repocad/util/event/ModifierKeys.scala
@@ -1,5 +1,7 @@
 package com.repocad.util.event
 
+import org.scalajs.dom.ModifierKeyEvent
+
 /**
   * ModifierKeys used to match further information in a given event.
   * Arguments are listed as follows: Shift - Control - Alt
@@ -25,5 +27,15 @@ object ModifierKeys {
     * An instance of [[ModifierKeys]] where Alt is pressed.
     */
   val Alt = ModifierKeys(false, false, true)
+
+  /**
+    * Creates a modifier keys domain object informing which modifiers were active when the given event was fired.
+    *
+    * @param e The event containing the information about the modifiers.
+    * @return A [[ModifierKeys]] instance.
+    */
+  def apply(e: ModifierKeyEvent): ModifierKeys =
+    new ModifierKeys(e.shiftKey, e.ctrlKey, e.altKey)
+
 
 }
